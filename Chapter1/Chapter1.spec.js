2975b139486a2fa02cf42d829dfba42100b6204f
silva.raphael.dev <silva.raphael.dev@hotmail.com>
2020-05-06 20:42:12 -0300

    Init the project, I have copied the example from the book and installed mocha and chai to create the tests, there is a simple test in the project to assert that the test framework is working properly

---
+++ Chapter1/Chapter1.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var expect = require("chai").expect;
+
+describe('sum', function() {
+  it('should be 2', function() {
+    const result = 1 + 1;
+
+    expect(result).to.equal(2);
+  });
+});
