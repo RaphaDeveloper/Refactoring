3ebd5f6e0a1a7b0fe855e9df8d6a9715bbb4072c
silva.raphael.dev <silva.raphael.dev@hotmail.com>
2020-05-06 21:41:05 -0300

    Implements the first test thaw will help me to recator the code with the confidence that nothing will be broken

--- Chapter1/Chapter1.spec.js
+++ Chapter1/Chapter1.spec.js
@@ -1,11 +1,43 @@
 'use strict';
 
-var expect = require("chai").expect;
+const expect = require("chai").expect;
+const statement = require('./Chapter1').statement;
 
-describe('sum', function() {
-  it('should be 2', function() {
-    const result = 1 + 1;
+describe('statement', function() {
+  it('should create the report', function() {
+    let expectedResult = 'Statement for BigCo\n';
+    expectedResult += '  Hamlet: $650.00 (55 seats)\n';
+    expectedResult += '  As You Like It: $580.00 (35 seats)\n';
+    expectedResult += '  Othello: $400.00 (25 seats)\n';
+    expectedResult += 'Amount owed is $1,630.00\n';
+    expectedResult += 'You earned 37 credits\n';
 
-    expect(result).to.equal(2);
+    const plays = {
+      "hamlet": {"name": "Hamlet", "type": "tragedy"},
+      "as-like": {"name": "As You Like It", "type": "comedy"},
+      "othello": {"name": "Othello", "type": "tragedy"}
+    };
+
+    const invoice = {
+      "customer": "BigCo",
+      "performances": [
+        {
+          "playID": "hamlet",
+          "audience": 55
+        },
+        {
+          "playID": "as-like",
+          "audience": 35
+        },
+        {
+          "playID": "othello",
+          "audience": 25
+        }
+      ]
+    };
+
+    const result = statement(invoice, plays);
+
+    expect(result).to.equal(expectedResult);
   });
 });
